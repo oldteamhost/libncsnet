dac43e985e85d56bbf503cde70bd4571a272d92a
lomaster <artemiykastapro@gmail.com>
2024-06-10 19:42:31 +0300

    update

--- tests/url.c
+++ tests/url.c
@@ -1,36 +1,53 @@
+/*
+ * Copyright (c) 2024, oldteam. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "../ncsnet/url.h"
 
-int i = 1;
-
-void test_url(const char *url)
+void usage(char **argv)
 {
-  url_t *u = NULL;
-
-  u = url_from_str(url);
-  if (!u)
-    return;
-
-  char res[url_len(u)];
-  url_to_str(u, res, url_len(u));
-  printf("probe %s\n", url);
-  //  assert(strcmp(url, res) == 0);
-  url_print(u);
-    
-  if (u)
-    url_free(u);
+  printf("Usage: %s <url1> <url2> <...,>\n", argv[0]);
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
-  test_url("ftp://ftp.is.co.za/rfc/rfc1808.txt");
-  test_url("sip:911@pbx.mycompany.com");
-  test_url("file://Users/John/Documents/Projects/Web/MyWebsite/about.html");
-  test_url("urn:oasis:names:specification:docbook:dtd:xml:4.1.2");
-  test_url("https://john.doe@www.example.com:1234/forum/questions/?tag=networking&order=newest#top");
-  test_url("https://john.doe@www.example.com:1234/forum/questions/?tag=networking&order=newest#:~:text=whatever");
-  test_url("tel:+1-816-555-1212");
-  test_url("ftp://sdf@gdf.com/");
-  test_url("mailto:John.Doe@example.com");
-  return 0;
+  int i = 1;
+  url_t *u = NULL;
+  if (argc <= 1)
+    usage(argv);
+  
+  for (; i < argc; i++) {
+    u = url_from_str(argv[i]);
+    if (!u)
+      printf("Failed format...,\n");
+    else {
+      url_print(u);
+      url_free(u);
+    }
+  }
 }
